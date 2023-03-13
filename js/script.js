b3bdcd5842a4e254c7f6a10a7e42b5554dbe1b27
domenicobacoli <domenico.bacoli89@gmail.com>
2023-03-13 16:25:06 +0100

    feat: add Bonus

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ const { createApp } = Vue
 
   createApp({
     data() {
+
       return {
-        message: 'Benvenuto!',
+        title: 'Titolo inserito da',
         name: "Domenico",
         lastName : "Bacoli",
-        
+        image: "https://fastly.picsum.photos/id/26/400/300.jpg?hmac=kT8L13wPBGnPU46rd2Qb6ToAR1O9ML5UvdYTJS3Y7pU",
+        nameLastNameColor: "red",
       }
+
     }
   }).mount('#app')
